49e49e50a278ac495d65b45c780b7f287b8a2cd8
Michal Fousek <chlup@chlup.info>
2023-05-04 12:23:27 +0200

    Add project and configure launch screen

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  CatsLibrary
+//
+//  Created by Michal Fousek on 04.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
